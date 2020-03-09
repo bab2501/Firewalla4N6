7988a94628d4705ab9c0ed2174e2e780fe0c389a
Alexander Blaauwgeers <alexander.blaauwgeers@os3.nl>
2020-03-09 15:02:32 +0100

    add bit rate bout

--- AutoImgr.sh
+++ AutoImgr.sh
@@ -1,3 +1,3 @@
 nc -lup 7654 | dd of=/tmp/image20200305_$(date +%s).raw&
-echo "dd if=/dev/mmcblk0 | nc -u desktop-30.students.os3.nl 7654" | picocom -qrix 1000 /dev/ttyUSB0 >> output.txt
+sudo echo "dd if=/dev/mmcblk0 | nc -u desktop-30.students.os3.nl 7654" | picocom -b 115200 -qrix 1000 /dev/ttyUSB0 >> output.txt
 watch -n 1 ls -la /tmp
