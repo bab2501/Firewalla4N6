f01f3b33ea7b6529bbae84f22c68d13d25ed3ff2
Alexander Blaauwgeers <alexander.blaauwgeers@os3.nl>
2020-03-12 18:35:10 +0100

    js local

--- F4N6php/alarm.php
+++ F4N6php/alarm.php
@@ -29,8 +29,8 @@ echo '<!DOCTYPE html>
 	
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<link rel="stylesheet" href="/resources/demos/style.css">
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script src="/static/js/jquery-1.12.4.js"></script>
+	<script src="/static/js/jquery-ui.js"></script>
 	<script>
 		$( function() {
 			$( "#tablenames" ).accordion({
